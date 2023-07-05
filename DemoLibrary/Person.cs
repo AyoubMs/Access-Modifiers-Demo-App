a3a9ab2713c89de7dd081e9727e0c5125995323f
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:03:10 +0200

    add app

---
+++ DemoLibrary/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string _ssn;
+
+        public string SSN
+        {
+            get
+            {
+                return $"***-**-{_ssn.Substring(_ssn.Length - 4)}";
+            }
+            set
+            {
+                _ssn = value;
+            }
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hello {FirstName}");
+        }
+    }
+}
